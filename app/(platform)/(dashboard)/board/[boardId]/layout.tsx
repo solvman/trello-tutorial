c371604ddf3d078da0077398f05daaf0ce7a2885
solvman <solvman@gmail.com>
2023-12-07 22:21:00 -0600

    Add board page

--- app/(platform)/(dashboard)/board/[boardId]/layout.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/layout.tsx
@@ -1,6 +1,67 @@
-const BoardIdLayout = ({ children }: { children: React.ReactNode }) => {
+import { notFound, redirect } from "next/navigation";
+
+import { auth } from "@clerk/nextjs";
+import { db } from "@/lib/db";
+
+import { BoardNavbar } from "./_components/board-navbar";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: {
+    boardId: string;
+  };
+}) {
+  const { orgId } = auth();
+
+  if (!orgId) {
+    return {
+      title: "Board",
+    };
+  }
+
+  const board = await db.board.findUnique({
+    where: {
+      id: params.boardId,
+      orgId,
+    },
+  });
+
+  return {
+    title: board?.title || "Board",
+  };
+}
+
+const BoardIdLayout = async ({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { boardId: string };
+}) => {
+  const { orgId } = auth();
+
+  if (!orgId) {
+    redirect("/select-org");
+  }
+
+  const board = await db.board.findUnique({
+    where: {
+      id: params.boardId,
+      orgId,
+    },
+  });
+
+  if (!board) {
+    notFound();
+  }
+
   return (
-    <div>
+    <div
+      className="relative h-full bg-no-repeat bg-cover bg-center"
+      style={{ backgroundImage: `url(${board.imageFullUrl})` }}
+    >
+      <BoardNavbar />
       <main className="relative pt-28 h-full">{children}</main>
     </div>
   );
