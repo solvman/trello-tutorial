76dd9f22e5c8d86abf0b5137b17a6ad887370afc
solvman <solvman@gmail.com>
2023-12-08 19:33:20 -0600

    Add board navbar title

--- app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
@@ -1,3 +1,17 @@
-export const BoardNavbar = () => {
-  return <div>Board Navbar</div>;
+import { auth } from "@clerk/nextjs";
+import { Board } from "@prisma/client";
+import { BoardTitleForm } from "./board-title-form";
+
+interface BoardNavbarProps {
+  data: Board;
+}
+
+export const BoardNavbar = async ({ data }: BoardNavbarProps) => {
+  const { orgId } = auth();
+
+  return (
+    <div className="w-full h-14 z-[40] bg-black/50 fixed top-14 flex items-center px-6 gap-x-4 text-white">
+      <BoardTitleForm data={data} />
+    </div>
+  );
 };
